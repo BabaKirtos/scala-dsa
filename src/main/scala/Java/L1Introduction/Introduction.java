43e1c97a13b4ed632b74c088cfe2dbfe78540591
Abhishek Roy <abhiroy1993@gamil.com>
2024-07-28 01:04:00 +0530

    type promotion

--- src/main/scala/Java/L1Introduction/Introduction.java
+++ src/main/scala/Java/L1Introduction/Introduction.java
@@ -1,5 +1,7 @@
 package Java.L1Introduction;
 
+import java.util.Scanner;
+
 public class Introduction {
 
     public static void main(String[] args) {
@@ -10,7 +12,26 @@ public class Introduction {
         // When we reassign a to another value like 20, object 10 is cleared from
         // the heap memory by the garbage collector
         name = "Abhishek";
-
         System.out.println("Hi, my name is: " + name);
+
+        // To get input from the console
+        System.out.println("Please enter your roll number:");
+        Scanner input = new Scanner(System.in);
+        int rollNumber = input.nextInt();
+        System.out.println("Your roll number is: " + rollNumber);
+
+        // automatic type promotion
+        byte a = 40;
+        byte b = 50;
+        byte c = 100;
+
+        // a, b and c are automatically upgraded to int
+        int d = a * b / c;
+        System.out.println(d);
+
+        // ascii value of s will be printed
+        // Java uses unicode
+        int someChar = 's';
+        System.out.println(someChar);
     }
 }
