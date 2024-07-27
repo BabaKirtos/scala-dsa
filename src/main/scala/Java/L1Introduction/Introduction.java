932aaaa21d5d257eefc20ad378394bcb467a7bdc
Abhishek Roy <abhiroy1993@gamil.com>
2024-07-27 22:48:59 +0530

    about memory

--- src/main/scala/Java/L1Introduction/Introduction.java
+++ src/main/scala/Java/L1Introduction/Introduction.java
@@ -4,6 +4,13 @@ public class Introduction {
 
     public static void main(String[] args) {
 
-        System.out.println("Hi");
+        // In the below statement, a is the reference variable and 10 is the object
+        // a is stored in stack, whereas object 10 is stored in heap memory
+        String name = "Baba";
+        // When we reassign a to another value like 20, object 10 is cleared from
+        // the heap memory by the garbage collector
+        name = "Abhishek";
+
+        System.out.println("Hi, my name is: " + name);
     }
 }
