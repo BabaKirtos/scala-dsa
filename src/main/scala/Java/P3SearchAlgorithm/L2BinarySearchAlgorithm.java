671df5c4c4f0e415c16d2a2f77ac1d088dd18f06
Abhishek Roy <abhiroy1993@gmail.com>
2024-10-10 23:54:58 +0530

    Binary Search Algorithm updated logic

--- src/main/scala/Java/P3SearchAlgorithm/L2BinarySearchAlgorithm.java
+++ src/main/scala/Java/P3SearchAlgorithm/L2BinarySearchAlgorithm.java
@@ -11,44 +11,54 @@ public class L2BinarySearchAlgorithm {
 
         // In binary search we take the middle element of the sorted Array
         // For an Array with length 13 we take index 6
-        System.out.println(input[input.length / 2]);
+        // System.out.println(input[input.length / 2]);
         // If our target is 34 and the middle element value is 15
         // Then our target lies on the right hand side of the middle value
-        // We again get the value of the middle element between index 6 to 12 ie 9
-        // algo to find middle element = (start + end) / 2
-        System.out.println(input[(6 + 12) / 2]);
-        // We get the value 25 at index 8
-        // We again find the middle element - (8 + 12) / 2 = 10
-        // We find our target
-        System.out.println(input[(8 + 12) / 2]);
-        System.out.println(binarySearch(input, 34));
-        System.out.println(binarySearch(input, 2));
-        System.out.println(binarySearch(input, 41));
-        System.out.println(binarySearch(input, 99));
+        // We again get the value of the middle element between index 7 to 12 ie 9
+        // algo to find middle index = (start + end) / 2
+        // System.out.println(input[(7 + 12) / 2]);
+        // We get the value 25 at index 9
+        // We again find the middle element - (10 + 12) / 2 = 11
+        // System.out.println(input[(10 + 12) / 2]);
+        // We get the value 39 at index 11
+        // We again find the middle element - (10 + 10) / 2 = 20
+        // We find our target, 34
+        // System.out.println(input[(10 + 10) / 2]);
+        // Take note that when target is greater, we add 1 to start before
+        // finding the next middle index, and when it is less, we subtract
+        // 1 from end before finding the next middle index
+        System.out.println(binarySearch(input, 2)); // 0
+        System.out.println(binarySearch(input, 4)); // 1
+        System.out.println(binarySearch(input, 6)); // 2
+        System.out.println(binarySearch(input, 9)); // 3
+        System.out.println(binarySearch(input, 11)); // 4
+        System.out.println(binarySearch(input, 13)); // 5
+        System.out.println(binarySearch(input, 15)); // 6
+        System.out.println(binarySearch(input, 17)); // 7
+        System.out.println(binarySearch(input, 20)); // 8
+        System.out.println(binarySearch(input, 25)); // 9
+        System.out.println(binarySearch(input, 34)); // 10
+        System.out.println(binarySearch(input, 39)); // 11
+        System.out.println(binarySearch(input, 41)); // 12
+        System.out.println(binarySearch(input, 99)); // -1
+        System.out.println(binarySearch(input, -99));// -1
     }
-    
+
     static int binarySearch(int[] arr, int target) {
         int startIndex = 0;
         int endIndex = arr.length - 1;
         int middleIndex = (startIndex + endIndex) / 2;
-        while (middleIndex != 0 || middleIndex != arr.length - 1){
+        while (middleIndex >= 0 && middleIndex < arr.length) {
             if (arr[middleIndex] < target) {
-                System.out.println(" < " + middleIndex + " " + arr[middleIndex]);
-                startIndex = middleIndex;
+                startIndex = middleIndex + 1;
             }
             if (arr[middleIndex] > target) {
-                System.out.println(" > " + middleIndex + " " + arr[middleIndex]);
-                endIndex = middleIndex;
+                endIndex = middleIndex - 1;
             }
             if (arr[middleIndex] == target) {
-                System.out.println(" = " + middleIndex + " " + arr[middleIndex]);
                 return middleIndex;
             }
-            System.out.println(" start " + startIndex + " end " + endIndex);
             middleIndex = (startIndex + endIndex) / 2;
-            if (startIndex == arr.length - 2) {
-                middleIndex += 1;
-            }
         }
         return -1;
     }
