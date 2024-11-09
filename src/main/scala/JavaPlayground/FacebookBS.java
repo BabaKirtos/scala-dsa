0943c2c1635d5460fa45c3f4b5f99fd4f3a5fe87
Abhishek Roy <abhiroy1993@gmail.com>
2024-11-09 21:39:28 +0530

    FacebookBS better algo

--- src/main/scala/JavaPlayground/FacebookBS.java
+++ src/main/scala/JavaPlayground/FacebookBS.java
@@ -15,43 +15,38 @@ public class FacebookBS {
         // This would mean running binary search twice
         // O(2log n) ~ O(log n)
         int[] input = {2, 5, 5, 5, 7, 7, 7, 7, 8, 8, 9, 10, 12};
-        System.out.println(Arrays.toString(facebookBS(input, 10)));
+        System.out.println(Arrays.toString(facebookBS(input, 7)));
     }
 
-    public static int firstOccurrence(int[] arr, int target) {
+    public static int findOccurrenceIndex(int[] arr, int target, boolean isFirst) {
+        int ans = -1;
         int start = 0;
         int end = arr.length - 1;
-        // first occurrence index
         while (start <= end) {
             int mid = start + ((end - start) / 2);
-            if (target == arr[mid] || target < arr[mid]) {
+            if (target > arr[mid]) {
+                start = mid + 1;
+            } else if (target < arr[mid]) {
                 end = mid - 1;
             } else {
-                start = mid + 1;
+                // save the mid-value
+                ans = mid;
+                // force to check left or right
+                // till condition is violated
+                if (isFirst) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
             }
         }
-        return start;
-    }
-
-    public static int lastOccurrence(int[] arr, int target) {
-        int start = 0;
-        int end = arr.length - 1;
-        // last occurrence index
-        while (start <= end) {
-            int mid = start + ((end - start) / 2);
-            if (target == arr[mid] || target > arr[mid]) {
-                start = mid + 1;
-            } else {
-                end = mid - 1;
-            }
-        }
-        return end;
+        return ans;
     }
 
     public static int[] facebookBS(int[] arr, int target) {
         int[] temp = new int[2];
-        temp[0] = firstOccurrence(arr, target);
-        temp[1] = lastOccurrence(arr, target);
+        temp[0] = findOccurrenceIndex(arr, target, true);
+        temp[1] = findOccurrenceIndex(arr, target, false);
         return temp[0] - temp[1] == 1 ? new int[]{-1, -1} : temp;
     }
 }
